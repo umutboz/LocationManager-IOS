9fd7c0090d233a1140f69fc0fe24e7426200ec95
02483564 <umut.boz@kocsistem.com.tr>
2019-02-27 14:25:43 +0300

    github version

--- Pods/Target Support Files/Pods-LocationManagerTestApp/Pods-LocationManagerTestApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LocationManagerTestApp : NSObject
-@end
-@implementation PodsDummy_Pods_LocationManagerTestApp
-@end
--- Pods/Target Support Files/Pods-LocationManagerTestAppTests/Pods-LocationManagerTestAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LocationManagerTestAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_LocationManagerTestAppTests
-@end
